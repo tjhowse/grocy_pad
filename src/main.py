4391094c6a548c2ffbd62def1135bb5b580142f6
tjhowse <tjhowse@gmail.com>
2021-07-23 17:37:44 +1000

    .

--- src/main.py
+++ src/main.py
@@ -2,7 +2,11 @@
 from m5stack_ui import *
 import time
 from i2c_kb import i2c_kb
+from secrets import ssid, password
 
+import wifiCfg
+
+wifiCfg.doConnect(ssid, password)
 
 screen = M5Screen()
 screen.clean_screen()
@@ -10,18 +14,8 @@ textarea = M5Textarea()
 
 keyboard = i2c_kb()
 
-# read once
-print("Key value:", end='')
-print(keyboard.read())
-
-# callback
 def keyboard_cb(value):
   print("Key value:", end='')
   print(value)
 
 keyboard.callback(keyboard_cb)
-# while True:
-#     time.sleep(0.1)
-#     a = keyboard.read()
-#     if a != b'\x00':
-#         print(a)
