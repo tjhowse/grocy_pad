d4cc768a5fbf43d7bc8a238c37058308ad699a71
tjhowse <tjhowse@gmail.com>
2021-07-26 22:10:30 +1000

    Improving, slowly.

--- src/main.py
+++ src/main.py
@@ -4,37 +4,45 @@ import time
 from i2c_kb import i2c_kb
 from secrets import grocy_api_key, grocy_domain
 from grocy_api import grocy_api
-import micropython
-micropython.alloc_emergency_exception_buf(100)
 
+def manage_input_box(kb, textbox):
+    kb.poll()
+    if kb.new:
+        textbox.setText(kb.get_buffer_as_string())
+
+# return
 screen = M5Screen()
 screen.clean_screen()
-textarea = M5Textarea()
-
-print("API Initialising")
+textarea = M5Textarea(text="Syncing with Grocy...", x=0, y=0, w=320,h=32)
 g = grocy_api(grocy_api_key, grocy_domain)
-print("Starting sync")
-# g.sync()
-print("Sync done")
+g.sync()
+textarea.set_text("Sync done.")
 keyboard = i2c_kb(interrupt=None)
 
 search_results = ""
+screen.clean_screen()
+buffer_text = M5Textarea( x=0, y=240-32, w=320, h=32)
+product_list = M5List(x=0, y=0)
+product_list.set_size(260,240-32)
+rows = 0
 
 print("Looping")
+keyboard.new = True
 while True:
     keyboard.poll()
     if keyboard.new:
         buffer = keyboard.get_buffer_as_string()
-        print("Got keyboard buffer: {}".format(buffer))
-        # textarea.set_text(buffer)
-        search_results = ""
-        print("Looking for products")
+        buffer_text.set_text(buffer)
+        while rows > 0:
+            product_list.remove_label_index(0)
+            rows -= 1
+        # product_list = M5List(x=0, y=0)
+        # product_list.set_size(260,240-32)
         for product in g.search_products_by_name(buffer):
-            search_results += product['name'] + "\n"
-        print("Writing results to textarea: {}".format(search_results))
-        # screen.clean_screen()
-        # textarea = M5Textarea()
-        textarea.set_text(search_results)
+            product_list.add_label(product['name'])
+            rows += 1
+            # pass
+
     # time.sleep(0.1)
 
 
